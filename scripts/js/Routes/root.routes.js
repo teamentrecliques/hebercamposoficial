a56e253a7f1a4f0f1df74119b2b44e7c52e29d05
Gabriel Barreto <barreto-gabriel@outlook.com>
2017-09-13 17:14:06 -0300

    [add] injection homeController in home route

--- scripts/js/Routes/root.routes.js
+++ scripts/js/Routes/root.routes.js
@@ -2,5 +2,6 @@ angular.module('hebercampos').config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'views/pages/home.html',
+            controller: 'homeController'
         });
 });
