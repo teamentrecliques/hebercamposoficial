17f818b5c625935044f8606ea1c466aa8bf45156
Gabriel Barreto <barreto-gabriel@outlook.com>
2017-09-14 18:50:16 -0300

    [add] discography section

--- scripts/js/Values/Mocks/discrography.mock.js
+++ scripts/js/Values/Mocks/discrography.mock.js
@@ -1,17 +1,17 @@
 angular.module('hebercampos').value('discography', {
     1: {
         title: "Amor que me constrange",
-        cover: "",
+        cover: "images/discography/1.jpg",
         link: ""
     },
     2: {
         title: "Águas do céu",
-        cover: "",
+        cover: "images/discography/2.jpg",
         link: ""
     },
     3: {
         title: "Meu Deus tem nome",
-        cover: "",
+        cover: "images/discography/3.jpg",
         link: ""
     }
 });
---
+++ scripts/js/Values/Mocks/singles.mock.js
@@ -0,0 +1,10 @@
+angular.module('hebercampos').value('singles', {
+	1: {
+		title: 'Mãe do Emanuel',
+		cover: 'images/singles/1.png'
+	},
+	2: {
+		title: 'O Segredo de Maria',
+		cover: 'images/singles/2.png'
+	}
+});
