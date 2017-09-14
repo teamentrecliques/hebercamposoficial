b9bdff14d16fbd20f94347ebb439cd749fbe1f44
Gabriel Barreto <barreto-gabriel@outlook.com>
2017-09-14 18:57:30 -0300

    [add] toggle discography section container function

--- scripts/js/Controllers/home.controller.js
+++ scripts/js/Controllers/home.controller.js
@@ -1,3 +1,14 @@
-angular.module('hebercampos').controller('homeController', function($scope, about) {
+angular.module('hebercampos').controller('homeController', function($scope, about, singles, discography) {
     $scope.about = about;
+    $scope.data = discography;
+    $scope.seeSingles = false;
+
+    $scope.toggleContainers = function(status) {
+        $scope.seeSingles = status;
+        if (status) {
+            $scope.data = singles;
+        } else {
+            $scope.data = discography;
+        }
+    };
 });
